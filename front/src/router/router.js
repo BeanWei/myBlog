923e64beb7b260fdec0f295128aefb7a527ad98b
BeanWei <157318439@qq.com>
2018-03-24 01:01:11 +0800

    添加文章爬取的前端轮播页面组件。采用阅读全文和上拉的方式

--- front/src/router/router.js
+++ front/src/router/router.js
@@ -3,6 +3,7 @@ import List from '@/components/Article/List.vue'
 import Detail from '@/components/Article/Detail.vue'
 import aboutMe from '@/components/About/aboutMe.vue'
 import Page404 from '@/components/ErroPage/Page404.vue'
+import Found from '@/components/Found/Found.vue'
 
 export const pageRouter = [
   {
@@ -22,12 +23,14 @@ export const pageRouter = [
       {path: 'blog/:slug',name: 'article',component: Detail}
     ]
   },
-  // {
-  //   path: '/news',
-  //   name: 'news',
-  //   redirect: '/news',
-  //   component: BlogLayout, 
-  // },
+  {
+    path: '/found',
+    component: BlogLayout, 
+    redirect: '/found',
+    children: [
+      {path: '/found',name: 'found',component: Found}
+    ] 
+  },
   {
     path: '/about',
     component: BlogLayout,
